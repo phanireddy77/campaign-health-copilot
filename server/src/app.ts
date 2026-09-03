8bdb01067b2ab7d989f8d7e40991faf1d3cca809
Phani Gudimetla <phani@mac.mynetworksettings.com>
2026-09-03 02:14:28 -0400

    Added basic logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,36 @@
 import express from "express";
 import cors from "cors";
 import db  from "./db/knex";
-import advertiserRoutes from "./routes/advertiserRouter";
+import advertiserRoutes from "./routes/advertiserRoutes";
+import campaignRoutes from "./routes/campaignRoutes";
+import lineRoutes from "./routes/lineRoutes";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api", (_req, res) => {
+  res.json({
+    service: "Campaign Health API",
+    version: "1.0.0",
+
+    endpoints: {
+      health: "/api/health",
+      advertisers: "/api/advertisers",
+      advertiserCampaigns:
+        "/api/advertisers/:advertiserId/campaigns",
+      campaign:
+        "/api/campaigns/:campaignId",
+      campaignLines:
+        "/api/campaigns/:campaignId/lines",
+      line:
+        "/api/lines/:lineId",
+      lineMetrics:
+        "/api/lines/:lineId/metrics",
+    }
+  });
+});
 
 app.get("/api/health", async (_req, res) => {
   try {
@@ -32,5 +57,7 @@ app.get("/api/health", async (_req, res) => {
 });
 
 app.use("/api/advertisers", advertiserRoutes);
+app.use("/api/campaigns", campaignRoutes);
+app.use("/api/lines", lineRoutes);
 
 export default app;
